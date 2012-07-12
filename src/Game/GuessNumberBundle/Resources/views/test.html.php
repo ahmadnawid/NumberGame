0705e28b63eff872983cb9154a2fe3d5f4b5a18a
ahmadnawid mustafazada <ahmadnawid.mz@gmail.com>
2012-07-12 13:39:49 +0200

    initial final commit

---
+++ src/Game/GuessNumberBundle/Resources/views/test.html.php
@@ -0,0 +1,24 @@
+<?php foreach ($view['assetic']->stylesheets(
+		array('@GameGuessNumberBundle/Resources/public/css/*')) as $url): ?>
+		<link rel="stylesheet" href="<?php echo $view->escape($url)?>" />
+<?php endforeach; ?>
+
+<?php $view->extend('GameGuessNumberBundle:GameViews:layout.html.php') ?>
+
+
+
+ 
+ <div id="body">
+	<div id="leftnav">
+		<div class="text">
+
+		 It took you <?php echo $numguess ?> turns to guess my number, which was <?php echo $number  ?>
+		 <br><br><br>
+		 
+		<a href="<?php echo $view['router']->generate('index')?>"> Play Guess the Number Again </a><br><br>
+		<a href="<?php echo $view['router']->generate('save', array('name' => $name, 'score' => $score, 'type' => $gtype), true)?>"> Save your score in the list of topscorers </a>
+		
+		
+		</div>	
+	</div>
+</div>
